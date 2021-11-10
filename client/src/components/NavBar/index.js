e97c1804cb021300b01f87320668eccf81d88843
theeshyguy <t.siopes00@yahoo.com>
2021-11-10 15:54:37 -0500

    final navbar update

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -28,7 +28,7 @@ class NavBar extends React.Component {
         }
         else {        
             return(
-            <Navbar variant={"dark"} className="mb-3" expand="md">
+            <Navbar variant={"dark"} className="mb-3 pb-0" expand="md">
                 <Container>
                     <Nav>
                         <div className="text-center" id="banner">
@@ -38,9 +38,9 @@ class NavBar extends React.Component {
                                         <Col xs={12} className="px-1"><Link to={`/`} title="Home" className="navIcon"><img src="/assets/images/thumbnails/undoctrinationShort.png" alt="Logo"/></Link></Col>
                                         <Col xs={12} md={4} className="d-xs-block d-md-none mt-2">
                                             <Container>
-                                                <Row className="align-items-end">
-                                                    <Col xs={10} className=""><img src="/assets/images/thumbnails/tagline.png" alt="Logo"/></Col>
-                                                    <Col xs={2} className=""><Navbar.Toggle aria-controls="main-navbar" className="mb-2"/></Col>
+                                                <Row className="align-items-center">
+                                                    <Col className="px-0"><img src="/assets/images/thumbnails/tagline.png" alt="Logo"/></Col>
+                                                    <Col xs={3} className="mb-2"><Navbar.Toggle aria-controls="main-navbar"/></Col>
                                                 </Row>
                                             </Container>
                                         </Col>
@@ -55,12 +55,10 @@ class NavBar extends React.Component {
                                                 <Col xs={6} md={4} lg={6} className="nav-link px-2"><Link to={`/platforms`}><Button variant={"theme"} className="btn-block"><strong>Platforms</strong></Button></Link></Col>
                                                 {/* <Col xs={12} md={4} lg={6} xl={4} className="nav-link px-2"><Link to={`/search`}><Button variant={"theme"} className="btn-block"><strong>Quotes</strong></Button></Link></Col> */}
                                                 <Col>
-                                                    <Form id="NavSearch" className="" onSubmit={this.handleSubmit}>
-                                                        <Row>
-                                                            <Col xs={10} md={11}><input type="text" id="searchTerm" placeholder="Search..." className="me-3 my-2 rounded" onChange={this.handleChange}></input></Col>
-                                                            <Col xs={1} className="text-white d-none d-md-inline ps-0"><FaSearch className="mt-3" type="submit" onClick={this.handleSubmit}/></Col>
-                                                            <Col xs={2} className="d-xs-inline d-md-none ps-0"><input className="mt-2 btn btn-theme" type="submit" value="Search"/></Col>
-                                                        </Row>
+                                                    <Form id="NavSearch" className="d-flex" onSubmit={this.handleSubmit}>
+                                                        <Col className="pe-2"><input type="text" id="searchTerm" placeholder="Search..." className="my-2 rounded" onChange={this.handleChange}></input></Col>
+                                                        <Col xs={2} lg={1} className="align-self-center text-white d-none d-md-block pb-1"><FaSearch type="submit" onClick={this.handleSubmit}/></Col>
+                                                        <Col xs={3} className="align-self-center d-xs-block d-md-none"><input className="btn btn-theme" type="submit" value="Search"/></Col>
                                                     </Form>
                                                 </Col>
                                             </Row>
