077d37649b472561f46d30d5c33e048931e9d2bc
theeshyguy <t.siopes00@yahoo.com>
2021-11-10 10:50:49 -0500

    updated navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -28,26 +28,33 @@ class NavBar extends React.Component {
         }
         else {        
             return(
-            <Navbar variant={"dark"} className="mb-3">
+            <Navbar variant={"dark"} className="mb-3" expand="md">
                 <Container>
                     <Nav>
-                        <div className="text-center mx-4" id="banner">
+                        <div className="text-center" id="banner">
                             <Row className="align-items-end">
-                                <Col x={12} md={6} lg={7}><Link to={`/`} title="Home" className="navIcon"><img src="/assets/images/thumbnails/undoctrination.png" alt="Logo"/></Link></Col>
-                                <Col x={12} md={6} lg={5}>
-                                    <Row className="mx-auto container mb-2">
-                                        <Col xs={6} className="nav-link"><Link to={`/freethinkers`}><Button variant={"theme"} className="btn-block"><strong>Freethinkers</strong></Button></Link></Col>
-                                        <Col xs={6} className="nav-link"><Link to={`/platforms`}><Button variant={"theme"} className="btn-block"><strong>Platforms</strong></Button></Link></Col>
-                                        {/* <Col xs={12} lg={8}>
-                                            <Form id="NavSearch" className="mb-2" onSubmit={this.handleSubmit}>
-                                                <Row>
-                                                    <Col xs={9} md={11}><input type="text" id="searchTerm" placeholder="Search..." className="me-3 my-2 rounded" onChange={this.handleChange}></input></Col>
-                                                    <Col xs={1} className="text-white d-none d-md-inline ps-0"><FaSearch className="mt-3" type="submit" onClick={this.handleSubmit}/></Col>
-                                                    <Col xs={3} className="d-xs-inline d-md-none ps-0"><input className="mt-2" type="submit" value="Search"/></Col>
-                                                </Row>
-                                            </Form>
-                                        </Col> */}
+                                <Col xs={12} lg={7}>
+                                    <Row className="align-items-end">
+                                        <Col xs={10} md={12}><Link to={`/`} title="Home" className="navIcon"><img src="/assets/images/thumbnails/undoctrination.png" alt="Logo"/></Link></Col>
+                                        <Col xs={2}><Navbar.Toggle aria-controls="main-navbar" className="mb-3"/></Col>
                                     </Row>
+                                </Col>
+                                <Col xs={12} lg={5}>
+                                    <Navbar.Collapse id="main-navbar">
+                                        <Row className="mx-auto container mb-2">
+                                            <Col xs={6} className="nav-link px-2"><Link to={`/freethinkers`}><Button variant={"theme"} className="btn-block"><strong>Freethinkers</strong></Button></Link></Col>
+                                            <Col xs={6} className="nav-link px-2"><Link to={`/platforms`}><Button variant={"theme"} className="btn-block"><strong>Platforms</strong></Button></Link></Col>
+                                            <Col xs={12}>
+                                                <Form id="NavSearch" className="" onSubmit={this.handleSubmit}>
+                                                    <Row>
+                                                        <Col xs={9} md={11}><input type="text" id="searchTerm" placeholder="Search..." className="me-3 my-2 rounded" onChange={this.handleChange}></input></Col>
+                                                        <Col xs={1} className="text-white d-none d-md-inline ps-0"><FaSearch className="mt-3" type="submit" onClick={this.handleSubmit}/></Col>
+                                                        <Col xs={3} className="d-xs-inline d-md-none ps-0"><input className="mt-2" type="submit" value="Search"/></Col>
+                                                    </Row>
+                                                </Form>
+                                            </Col>
+                                        </Row>
+                                    </Navbar.Collapse>
                                 </Col>
                             </Row>
                         </div>
